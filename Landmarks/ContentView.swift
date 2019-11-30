de28d7fcdd345fecaa7db0c03f47537d4f648f9c
d40man <d40man@126.com>
2019-11-30 17:15:26 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 周栋 on 2019/11/30.
+//  Copyright © 2019 d40man. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
